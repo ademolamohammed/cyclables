b0499385e17d0aacacaae4bab0f71f276cd52eaa
ademolamohammed <hammedmoh06@gmail.com>
2024-10-05 07:09:12 -0700

    axios added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from "./component/header/Header"
+import axios from 'axios';
 
 function App() {
+
+  const [data,setData] = useState([]);
+
+  axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(function (response) {
+    // handle success    
+    setData(response.data);
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+
+
+  })
+  .finally(function () {
+    // always executed
+    console.log("i'm finally and i'll always execute")
+
+  });
+
+
   return (
     <div>
       <Header />
+      {
+        data.map((item) => (
+          <div>
+            <h2>{item.id}</h2>
+            <p>{item.title}</p>
+
+          </div>
+        ))
+      }
     </div>
   );
 }
